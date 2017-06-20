5c6ce908b7f9c95fb92fa0e8b43134acba92c8dc
ahmedmhhh <a7medmhhh@hotmail.com>
2017-06-20 12:36:26 +0200

    Supporting Multiple Screen

--- app/src/main/java/com/example/internet/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/internet/calculatorapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.internet.calculatorapp
 
+import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
